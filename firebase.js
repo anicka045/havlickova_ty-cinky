8762c7107ebd501250f8d1de8467efecf180af41
anicka045 <havlanina@seznam.cz>
2025-05-25 17:02:05 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getDatabase, ref, push, get, child } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "TVŮJ_API_KEY",
+  apiKey: "AIzaSyCm73AHVHctySyU3x2n1WgATqWRLiccAJA",
   authDomain: "havlickova-ty-cinky.firebaseapp.com",
   databaseURL: "https://havlickova-ty-cinky-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "havlickova-ty-cinky",
   storageBucket: "havlickova-ty-cinky.appspot.com",
-  messagingSenderId: "TVŮJ_SENDER_ID",
-  appId: "TVŮJ_APP_ID"
+  messagingSenderId: "56449306745",
+  appId: "1:56449306745:web:e52c7d24296cb9cbac04a8"
 };
 
 const app = initializeApp(firebaseConfig);
